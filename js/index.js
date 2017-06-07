3d9aa1edc26fef8b2a455e9e600789751b171ff7
Jazzh5 <399162728@qq.com>
2017-06-07 17:17:02 +0800

    添加尾页logo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+﻿
 $(document).ready(function(){
     
 
@@ -618,4 +618,73 @@ function code(){
         $(this).text(str.toUpperCase())
     });
 
+    weixinShare()
+
 });
+
+function weixinShare() {
+$.ajax({
+    url : './weixinapi/api.php',
+    data : {
+        url: location.href.split('#')[0],
+        type: "signature"
+    },
+    dataType: "json",
+    success : function(result) {
+        if (result.code != 0) {
+            alert('获取签名出错');
+            return;
+        }
+        configWeixin(result.data)   
+    }
+});
+var setupWeixinShare = function (message) {
+                wx.onMenuShareTimeline(message);
+                wx.onMenuShareAppMessage(message);
+                wx.onMenuShareQQ(message);
+                wx.onMenuShareWeibo(message);
+                wx.onMenuShareQZone(message);
+        };
+
+        var configWeixin = function (options) {
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: options.appId, // 必填，公众号的唯一标识
+                timestamp: options.timestamp, // 必填，生成签名的时间戳
+                nonceStr: options.nonceStr, // 必填，生成签名的随机串
+                signature: options.signature,// 必填，签名，见附录1
+                jsApiList: [
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'hideMenuItems',
+                    'onMenuShareQZone'
+                ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
+        };
+
+        wx.ready(function () {
+            setupWeixinShare({
+                title: '寻找城市里的每一个幕后英雄', // 分享标题
+                desc: '这个H5里面，有每一个你', // 分享描述授人以鱼不如授人以渔，内创改变了她们
+                link: 'http://www.zgjrzj.com/lg/index.html', // 分享链接
+                imgUrl: 'http://www.zgjrzj.com/lg/imgs/logo.jpg', // 分享图标
+                type: '', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+            wx.hideMenuItems({
+                menuList: [
+                'menuItem:copyUrl',
+                'menuItem:originPage','menuItem:editTag',
+                'menuItem:share:email','menuItem:delete',
+                'menuItem:openWithQQBrowser','menuItem:openWithSafari'] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
+            });
+        });
+}
---
+++ weixinapi/config.php
@@ -0,0 +1,10 @@
+<?php
+    define('APP_ID', 'wxd22bcf9fb5e811ff');
+    define('APP_SECRET', '01ff9ed27f9a643148bd67eaf60d44a6');
+    define('DATA_PATH', './data/');
+    /*
+     * Environment
+     */
+    define('ENV', 'sae');
+    //define('ENV', 'dev');
+    define('SAE_DATA_PATH', 'saekv://');
